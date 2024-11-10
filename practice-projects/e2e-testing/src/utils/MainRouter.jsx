309a30eb96ac8bb056362438f1f0f1c489440924
Brett Pittman <brett.pittman@comporium.com>
2024-11-10 15:30:02 -0500

    Renamed root page

--- practice-projects/e2e-testing/src/utils/MainRouter.jsx
+++ practice-projects/e2e-testing/src/utils/MainRouter.jsx
@@ -5,8 +5,7 @@ import {
 } from "react-router-dom";
 import ThemeWrapper from '@/ThemeWrapper'
 import App from '@/App.jsx'
-import ErrorPage from '@pages/ErrorPage.jsx'
-import Root from "@pages/Root";
+import Root from "@pages/RootPage";
 
 const MainRouter = createBrowserRouter(createRoutesFromElements(
     <Route path="/" errorElement={<ErrorPage />}>
