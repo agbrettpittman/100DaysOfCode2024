6be56ad23432e8068ba20d3b4bde0055a56d5154
Brett Pittman <brett.pittman@comporium.com>
2024-11-11 18:27:46 -0500

    improved path aliasing

--- practice-projects/e2e-testing/vite.config.js
+++ practice-projects/e2e-testing/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         alias: {
             '@': '/src',
             '@pages': '/src/pages',
+            '@utils': '/src/utils',
             '@mui/styled-engine': '@mui/styled-engine-sc',
         },
     },
