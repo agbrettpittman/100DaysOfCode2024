d07f9d7a938fb8531e1d48084f797c7507c85b01
Brett Pittman <brett.pittman@comporium.com>
2024-11-11 19:15:52 -0500

    now capturing videos and screenshots of failures

--- practice-projects/e2e-testing/playwright.config.js
+++ practice-projects/e2e-testing/playwright.config.js
@@ -28,7 +28,9 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',  // Saves video of failed tests
 
     viewport: { width: 1920, height: 1080 },
   },
