b12328da7672a0486e09c3f0f052bfa6e4e854b5
Brett Pittman <brett.pittman@comporium.com>
2024-11-14 20:39:37 -0500

    removed unnecessary code from character page

--- practice-projects/e2e-testing/src/pages/CharacterPage.jsx
+++ practice-projects/e2e-testing/src/pages/CharacterPage.jsx
@@ -7,19 +7,13 @@ import useCharacter from '@utils/hooks/useCharacter'
 export default function CharacterPage() {
     const { id } = useParams()
 
-    const [Character, setCharacter] = useCharacter(id, {})
+    const [Character, setCharacter] = useCharacter(id)
     const DisplayCreation = moment(Character.creationDate).format('MMMM Do YYYY, h:mm:ss a')
-
-    const RandomNumber = Math.floor(Math.random() * 1000)
-    const NewCharacterName = `Test Character ${RandomNumber}`
 
     return (
         <div>
             <h1>{Character.name}</h1>
             <p class="creationDate">{DisplayCreation}</p>
-            <button onClick={() => setCharacter("name", NewCharacterName)}>
-                Set Name to {NewCharacterName}
-            </button>
         </div>
     )
 }
