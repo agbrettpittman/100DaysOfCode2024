67ec96ec02182e8f20d248717f7ebb4681f30fe6
Brett Pittman <brett.pittman@comporium.com>
2025-01-07 20:16:37 -0500

    📊 Updated database schema for widgets_PingPlotter_results to set default success value and allow null latency

--- practice-projects/network-event-monitor/backend/routers/widgetRouters/PingPlotter/utilities/db.py
+++ practice-projects/network-event-monitor/backend/routers/widgetRouters/PingPlotter/utilities/db.py
@@ -18,8 +18,8 @@ def initialize_database(cursor):
         CREATE TABLE IF NOT EXISTS widgets_PingPlotter_results (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             sendTime TEXT DEFAULT CURRENT_TIMESTAMP,
-            success INTEGER NOT NULL,
-            latency REAL NOT NULL,
+            success INTEGER NOT NULL DEFAULT 0,
+            latency REAL,
             hosts_id INTEGER NOT NULL,
             FOREIGN KEY (hosts_id) REFERENCES widgets_PingPlotter_hosts (id)
         )
